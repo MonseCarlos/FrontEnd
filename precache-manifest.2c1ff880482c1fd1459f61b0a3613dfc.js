aa553a954c7a222549e62c162958d7caf20997db
Raquel Monserrat Carlos Perez <monse@MacBook-Pro-de-Raquel.local>
2019-08-15 21:32:47 -0500

    Updates

---
+++ precache-manifest.2c1ff880482c1fd1459f61b0a3613dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f0b441f73d5b5dcc660b4c29e7b5ba",
+    "url": "/FrontEnd/index.html"
+  },
+  {
+    "revision": "f11246a2618df5abc065",
+    "url": "/FrontEnd/static/css/main.77e09e27.chunk.css"
+  },
+  {
+    "revision": "367d78de7f6628f9e2ff",
+    "url": "/FrontEnd/static/js/2.53575370.chunk.js"
+  },
+  {
+    "revision": "f11246a2618df5abc065",
+    "url": "/FrontEnd/static/js/main.b652998c.chunk.js"
+  },
+  {
+    "revision": "a7917d5d6d1df40d1ebe",
+    "url": "/FrontEnd/static/js/runtime~main.ff96218d.js"
+  }
+]);
